3bf06e1fcc237618ff92a66c5c57f5ed25b85b1e
SUBcharacter <farg557@gmail.com>
2025-04-23 17:33:49 +0900

    Reapply "25.04.22 8일차 커밋"
    This reverts commit ce7a31d0811bb86087d4fac0c44a56542d2d281c.

---
+++ CPP/Ch7/Character.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Skill.h"
+#include "Entity.h"
+
+class Character : public Entity
+{
+private:
+	 
+public:
+	void UseSkill(Skill& skill);
+	void UseHP();
+};
+
---
+++ CPP/Ch7/main.cpp
@@ -0,0 +1,11 @@
+#include "Skill.h"
+
+// A 개발자
+// B 개발자
+
+int main()
+{
+	Skill skill;
+
+	skill.Use();
+}
